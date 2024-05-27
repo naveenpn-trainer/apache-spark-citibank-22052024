c1d842ec036984b81203188ad548350421d9ce19
npntraining <npn.npntraining@gmail.com>
2024-05-27 13:15:11 +0530

    Add code

---
+++ kafka-for-developers/src/main/java/org/citibank/partitioning_mechanism/CustomPartitioning.java
@@ -0,0 +1,31 @@
+package org.citibank.analytics.partitioning_mechanism;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+
+import java.util.Map;
+
+public class CustomPartitioning implements Partitioner {
+    @Override
+    public int partition(String s, Object key, byte[] bytes, Object value, byte[] bytes1, Cluster cluster) {
+        String keyAsString = key.toString();
+        if(keyAsString.equals("KAR")){
+            return 0;
+        } else if (keyAsString.equals("TN")) {
+            return 1;
+        }
+        else{
+            return 2;
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> map) {
+
+    }
+}

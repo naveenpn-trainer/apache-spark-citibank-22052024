798a57d0c22ec4627b76a7743d2f19bbf70318a7
npntraining <npn.npntraining@gmail.com>
2024-05-27 12:53:25 +0530

    Add code

---
+++ kafka-for-developers/src/main/java/org/citibank/partitioning_mechanism/Approach03.java
@@ -0,0 +1,41 @@
+package org.citibank.analytics.partitioning_mechanism;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.citibank.analytics.AppConfigs;
+
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Approach03 {
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.CLIENT_ID_CONFIG, AppConfigs.APPLICATION_ID);
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, AppConfigs.BOOTSTRAP_SERVERS);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        KafkaProducer<Integer, String> producer = new KafkaProducer<>(properties);
+
+        Scanner input = new Scanner(System.in);
+        String message = null;
+        String key = null;
+        do {
+            System.out.println("Enter key");
+            key = input.nextLine();
+            System.out.println("Enter message");
+            message = input.nextLine();
+            int partitionNum;
+            System.out.println("Enter partition no");
+            partitionNum = Integer.parseInt(input.nextLine());
+            if (!message.equals("quit")) {
+                ProducerRecord producerRecord = new ProducerRecord(AppConfigs.TOPIC_MY_DEMO_TOPIC, partitionNum, key, message);
+                producer.send(producerRecord);
+            }
+        } while (!message.equals("quit"));
+        producer.close();
+    }
+}
